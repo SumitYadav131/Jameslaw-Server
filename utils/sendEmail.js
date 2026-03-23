3dedfaaa723765930845575fd6eea4f64e1e076c
Sumit Yadav <sumit@mydevitsolutions.com>
2026-03-23 11:18:54 +0530

    mailer update

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -12,12 +12,13 @@ export const sendEmail = async (to, subject, html) => {
 
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
-        }
+        },
+        connectionTimeout: 10000, // 10 sec
     });
 
     await transporter.sendMail({
