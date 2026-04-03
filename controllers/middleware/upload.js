f2903cc7cddd4aa2442731b54b741b873627b942
Sumit Yadav <sumit@mydevitsolutions.com>
2026-04-03 12:57:15 +0530

    filter file type

--- controllers/middleware/upload.js
+++ controllers/middleware/upload.js
@@ -22,4 +22,16 @@ const storage = multer.diskStorage({
 export const upload = multer({
     storage,
     limits: { fileSize: 15 * 1024 * 1024 },
+    fileFilter: (req, file, cb) => {
+        const allowedTypes = [
+            "application/pdf",
+            "image/jpeg",
+            "image/png",
+        ];
+        if (allowedTypes.includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb(new Error("Invalid file type"), false);
+        }
+    }
 });
