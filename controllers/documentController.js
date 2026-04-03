e730b9c13c6f9e78412024a8ea2862e61a256009
Sumit Yadav <sumit@mydevitsolutions.com>
2026-04-03 12:51:53 +0530

    document controller update

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -2,12 +2,13 @@ import Document from "../models/Document.js";
 export const uploadDocument = async (req, res) => {
     try {
         const userId = req.user.userId;
+        const { docName } = req.body;
         if (!req.file) {
             return res.status(400).json({ message: "No file uploaded" });
         }
         const doc = await Document.create({
             userId,
-            fileName: req.file.originalname,
+            fileName: docName || req.file.filename,
             filePath: req.file.path,
             fileType: req.file.mimetype,
             fileSize: req.file.size,
