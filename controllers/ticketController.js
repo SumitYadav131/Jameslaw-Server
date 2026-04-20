0769ef5e2c589b11ee456a27f99a02c562d04509
Sumit Yadav <sumit@mydevitsolutions.com>
2026-04-20 10:25:08 +0530

    sort the tickets in decending order

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -65,7 +65,7 @@ export const createTicket = async (req, res) => {
 export const getTickets = async (req, res) => {
     try {
 
-        const tickets = await Ticket.find();
+        const tickets = await Ticket.find().sort({ createdAt: -1 });
         res.status(200).json({
             message: "Tickets Fetched Successfully",
             tickets
