32ddaa25ad1f4ad1dfe8ab6b4c8bfb94b11d277e
Sumit Yadav <sumit@mydevitsolutions.com>
2026-05-04 11:11:11 +0530

    change in getting ticket

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -74,7 +74,7 @@ export const createTicket = async (req, res) => {
 
 export const getTickets = async (req, res) => {
     try {
-        const tickets = await Ticket.find().sort({ createdAt: -1 });
+        const tickets = await Ticket.find({ userId: req.user.id }).sort({ createdAt: -1 });
         res.status(200).json({
             message: "Tickets Fetched Successfully",
             tickets
